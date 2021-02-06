46637a152d599163ecbb73a8ea94813edccbe852
congshu <wofeishenling@hotmail.com>
2021-02-06 15:34:47 +0800

    .

---
+++ 链表操作/leetcode25.cpp
@@ -0,0 +1,57 @@
+#include<map>
+using namespace std;
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    // 翻转一个子链表，并且返回新的头与尾
+    pair<ListNode*, ListNode*> myReverse(ListNode* head, ListNode* tail) {
+        ListNode* pre = tail->next;
+        ListNode* cur = head;
+        ListNode* nex = NULL;
+        while (pre != tail) {         //node0->node1->node2->node3->NULL
+            nex = cur->next;          //head                 tail   pre
+            cur->next = pre;          //cur      nex
+            pre = cur;
+            cur = nex;
+        }
+        return {tail, head};
+    }
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* hair = new ListNode(0);
+        hair->next = head;
+        ListNode* pre = hair;
+
+        while (head) {
+            ListNode* tail = pre;
+            // 查看剩余部分长度是否大于等于 k
+            for (int i = 0; i < k; ++i) {
+                tail = tail->next;
+                if (!tail) {
+                    return hair->next;
+                }
+            }
+            ListNode* nex = tail->next;
+            // 这里是 C++17 的写法，也可以写成
+            // pair<ListNode*, ListNode*> result = myReverse(head, tail);
+            // head = result.first;
+            // tail = result.second;
+            tie(head, tail) = myReverse(head, tail);
+            // 把子链表重新接回原链表
+            pre->next = head;
+            tail->next = nex;
+            pre = tail;
+            head = tail->next;
+        }
+
+        return hair->next;
+    }
+};
